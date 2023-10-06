43bcb88597dfa46a8bbf30e76da368a750eef285
ChrisCablish <112437010+ChrisCablish@users.noreply.github.com>
2023-10-06 13:53:29 -0500

    intital commit

---
+++ src/test/java/com/example/LibraryMVC/LibraryMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LibraryMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
